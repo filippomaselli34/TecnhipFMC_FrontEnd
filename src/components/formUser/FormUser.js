7d9f1eeaaf31471c2ad1056c9aff6748d2a26675
Afmjuniors <afmjuniors@gmail.com>
2023-04-28 17:47:53 -0300

    fix: mudança de porta de 3001 pra 3002 e porta 3004 para back

--- src/components/formUser/FormUser.js
+++ src/components/formUser/FormUser.js
@@ -11,45 +11,46 @@ import { Container, UserGroup, GroupWrapper, InfoContainer } from './FormUser.st
 
 
 
-const FormUser = ({setModalUser}) => {
+const FormUser = ({ setModalUser }) => {
 
     const [editPassword, setEditPassword] = useState(false)
     const [pass, setPass] = useState("")
     const [email, setEmail] = useState("")
+    const[isEmailEdit,setIsEmailEdit]= useState(false)
 
-    const {roleStr, idUser,emailUser,userName,autho} = useContext(GlobalContext)
+    const { roleStr, idUser, emailUser, userName, autho } = useContext(GlobalContext)
 
-    const handleClickEditEmail = () =>{
+    const handleClickEditEmail = () => {
 
-        if(email.match(regexEmail)){
+        if (email.match(regexEmail)) {
 
             const body = {
                 email
             }
             handleRequisitionEdit(body)
-        }else{
+        } else {
             window.alert("Formato do email invalido")
         }
     }
-    const handleClickEditPassword = () =>{
-        if(pass.match(regexPassword)){
+    const handleClickEditPassword = () => {
+        if (pass.match(regexPassword)) {
 
             const body = {
-                password:pass
+                password: pass
             }
             handleRequisitionEdit(body)
-          setPass('')
-          setEditPassword(false)
+            setPass('')
+            setEditPassword(false)
 
-        }else{
+        } else {
             window.alert("O password de ter de 8 a 12 caracteres, contendo pelo monos 1 letra Maiuscula, 1 letra minuscula e um caracter especial")
         }
     }
 
 
-    const handleRequisitionEdit = async (body)=>{
+    const handleRequisitionEdit = async (body) => {
         try {
-            const response = await axios.put(BASE_URL+"users",body,headers)
+            const response = await axios.put(BASE_URL + "users", body, headers)
             console.log(response)
         } catch (error) {
             window.alert(error.response)
@@ -66,28 +67,41 @@ const FormUser = ({setModalUser}) => {
                 <h2 className='role-tirle'>{roleStr}</h2>
                 <p className='id-title'>{idUser}</p>
             </UserGroup>
-            <button onClick={()=>setModalUser(0)} className='btn btn-dark clode-btn'>X</button>
+            <button onClick={() => setModalUser(0)} className='btn btn-dark clode-btn'>X</button>
             <InfoContainer>
 
                 <GroupWrapper>
                     <label>Email:</label>
                     <div className='group-sec'>
-                        <input placeholder={emailUser} value={email} onChange={(e)=>setEmail(e.target.value)}  />
-                        <button  type='button' className='btn btn-link' onClick={handleClickEditEmail}>Editar</button>
+                        {
+                            !isEmailEdit ?
+                                <>
+                                    <p>{emailUser}</p>
+                                    <button type='button' className='btn btn-link' onClick={() => setIsEmailEdit(true)}>Editar</button>
+                                </> : <>
+                                    <input placeholder={emailUser} value={email} onChange={(e) => setEmail(e.target.value)} />
+                                    <button type='button' className='btn btn-success' onClick={handleClickEditEmail}>Salvar</button>
+                                    <button type='button' className='btn btn-danger btn-cancel' onClick={() => {
+                                        setEmail("")
+                                        setIsEmailEdit(false)
+                                    }} >Cancelar</button>
+                                </>
+                        }
                     </div>
                 </GroupWrapper>
                 {
                     editPassword ?
                         <GroupWrapper>
-                            <p className='edit-pass'  onClick={()=>setEditPassword(false)}>Cancelar edição</p>
+                            <p className='edit-pass' onClick={() => setEditPassword(false)}>Cancelar edição</p>
                             <label>Senha:</label>
                             <div className='group-sec'>
-                                <input placeholder='Nova senha' value={pass} onChange={(e)=>setPass(e.target.value)} />
-                                <button type='button' className='btn btn-success'  onClick={handleClickEditPassword} >Salvar</button>
+                                <input placeholder='Nova senha' value={pass} onChange={(e) => setPass(e.target.value)} />
+                                <button type='button' className='btn btn-success' onClick={handleClickEditPassword} >Salvar</button>
+
                             </div>
                         </GroupWrapper> :
                         <GroupWrapper>
-                            <p className='edit-pass' onClick={()=>setEditPassword(true)}  >Editar Senha</p>
+                            <p className='edit-pass' onClick={() => setEditPassword(true)}  >Editar Senha</p>
                         </GroupWrapper>
 
                 }
